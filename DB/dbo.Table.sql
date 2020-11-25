e42cf5d3eeb28f5042c147d1703a6cc48fcefbb4
Mladen Janjic <mladen@ftn.edu.rs>
2020-11-25 18:54:20 +0100

    Kreiran .sql fajl za tabelu

---
+++ DB/dbo.Table.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Student]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Name] NVARCHAR(100) NOT NULL, 
+    [IndexNumber] NVARCHAR(20) NOT NULL, 
+    [AverageMark] DECIMAL(16, 2) NULL
+)
